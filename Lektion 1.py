0ad252d690c83117559cb5f945cca8b7d83a71d9
Basse-byte <ekedahlsebastian@gmail.com>
2024-10-30 09:45:49 +0100

    Lektion 3 om att ändra strängar

--- Lektion 1.py
+++ Lektion 1.py
@@ -1,2 +1,44 @@
-x = 5
-print(x)
+# name="Sebastian"
+# age=30
+# temp=10, "Grader Ceclius"
+# control=(1=1)
+# print(name, age, temp, control)
+
+
+""" a=15
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+# Golvdivision? print(a+b)
+print(a//b)
+print(a**b) """
+
+"""
+greating="Hello, World!"
+
+print(greating[0])
+print(greating[1])
+print(greating[2])
+print(greating[3])
+print(greating[4])
+print(greating[5])
+print(greating[6])
+print(greating[7])
+print(greating[8])
+print(greating[9])
+print(greating[10])
+print(greating[11])
+print(greating[12])
+"""
+"""
+print(greating[7])
+print(greating[12])
+"""
+"""
+print(greating[::2])
+"""
+"""
+print(greating[::-1])
+"""
---
+++ Lektion 2.py
@@ -0,0 +1,36 @@
+""" Exponenter
+bas =2
+exponent = 3
+results = bas ** exponent
+print("Result is : " , results)
+"""
+
+""" Vilket räknesätt utförs först?
+results = 10 + 2 * 3
+print("Result is: ", results)
+"""
+
+""" Samma igen
+results = (10 + 2) * 3
+print("Result is: ", results)
+"""
+
+""" () * / + -
+a = 15
+b = 4
+c = 2
+results = (a-b) * c + b / c
+print("Result is: ", results)
+11 * 2 + 4 / 2
+"""
+
+# Arytmiska uträkningar med floats
+a = 5.5
+b = 2.0
+
+sums = a + b
+product = a * b
+print(sums)
+print(product)
+
+
